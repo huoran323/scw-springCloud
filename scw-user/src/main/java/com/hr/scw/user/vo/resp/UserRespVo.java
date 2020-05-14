847519f79fc7406c370370dc279f8c797d8c6bb8
xiaoran <303176152@qq.com>
2020-05-14 17:00:42 +0800

    submit

---
+++ scw-user/src/main/java/com/hr/scw/user/vo/resp/UserRespVo.java
@@ -0,0 +1,98 @@
+package com.hr.scw.user.vo.resp;
+
+import io.swagger.annotations.ApiModel;
+
+@ApiModel
+public class UserRespVo {
+	
+	//令牌 登陆后会分配给当前用户一个临时令牌值。以后对系统的任何访问必须携带这个令牌。否则拒绝访问
+	private String accessToken;
+
+    private String loginacct;
+
+    private String username;
+
+    private String email;
+
+    private String authstatus = "0";
+
+    private String usertype;
+
+    private String realname;
+
+    private String cardnum;
+
+    private String accttype;
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getLoginacct() {
+		return loginacct;
+	}
+
+	public void setLoginacct(String loginacct) {
+		this.loginacct = loginacct;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAuthstatus() {
+		return authstatus;
+	}
+
+	public void setAuthstatus(String authstatus) {
+		this.authstatus = authstatus;
+	}
+
+	public String getUsertype() {
+		return usertype;
+	}
+
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
+
+	public String getRealname() {
+		return realname;
+	}
+
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+
+	public String getCardnum() {
+		return cardnum;
+	}
+
+	public void setCardnum(String cardnum) {
+		this.cardnum = cardnum;
+	}
+
+	public String getAccttype() {
+		return accttype;
+	}
+
+	public void setAccttype(String accttype) {
+		this.accttype = accttype;
+	}
+}
